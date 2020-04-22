872b4c22eb7a864c0163f48dfef9e851db3b4b4b
Sharad Waghchaure <sharad.wangchaure@multiply.co.za>
2020-04-22 16:23:31 +0200

    splash added

---
+++ app/src/main/java/za/co/immedia/SplashActivity.java
@@ -0,0 +1,29 @@
+package za.co.immedia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+               startActivity(new Intent(SplashActivity.this,MainActivity.class));
+               finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
